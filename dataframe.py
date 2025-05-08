c1de23d425e8dbbee4b04f98d7dbd6eedc372118
PATRICK CHOI <76716959+PATRICK-KTWIZ@users.noreply.github.com>
2025-05-08 14:28:17 +0900

    Update dataframe.py

--- dataframe.py
+++ dataframe.py
@@ -842,10 +842,10 @@ def stats_df(merged_base_df):
     merged_base_df['ops'] = merged_base_df['obp'] + merged_base_df['slg']
     
     # 스트라이크존 관련 통계 (안전한 나눗셈)
-    mask_player = merged_base_df['player_name'] > 0
+    mask_player = merged_base_df['pitname'] > 0
     if mask_player.any():
-        merged_base_df.loc[mask_player, 'z%'] = merged_base_df.loc[mask_player, 'z_in'] / merged_base_df.loc[mask_player, 'player_name']
-        merged_base_df.loc[mask_player, 'inplay_pit'] = merged_base_df.loc[mask_player, 'inplay'] / merged_base_df.loc[mask_player, 'player_name']
+        merged_base_df.loc[mask_player, 'z%'] = merged_base_df.loc[mask_player, 'z_in'] / merged_base_df.loc[mask_player, 'pitname']
+        merged_base_df.loc[mask_player, 'inplay_pit'] = merged_base_df.loc[mask_player, 'inplay'] / merged_base_df.loc[mask_player, 'pitname']
     
     mask_z_in = merged_base_df['z_in'] > 0
     if mask_z_in.any():
@@ -858,7 +858,7 @@ def stats_df(merged_base_df):
     
     # 스트라이크존 밖 관련 통계 (안전한 나눗셈)
     if mask_player.any():
-        merged_base_df.loc[mask_player, 'o%'] = merged_base_df.loc[mask_player, 'z_out'] / merged_base_df.loc[mask_player, 'player_name']
+        merged_base_df.loc[mask_player, 'o%'] = merged_base_df.loc[mask_player, 'z_out'] / merged_base_df.loc[mask_player, 'pitname']
     
     mask_z_out = merged_base_df['z_out'] > 0
     if mask_z_out.any():
@@ -875,7 +875,7 @@ def stats_df(merged_base_df):
         merged_base_df.loc[mask_f_pitch, 'f_swing%'] = merged_base_df.loc[mask_f_pitch, 'f_swing'] / merged_base_df.loc[mask_f_pitch, 'f_pitch']
     
     if mask_player.any():
-        merged_base_df.loc[mask_player, 'swing%'] = merged_base_df.loc[mask_player, 'swing'] / merged_base_df.loc[mask_player, 'player_name']
+        merged_base_df.loc[mask_player, 'swing%'] = merged_base_df.loc[mask_player, 'swing'] / merged_base_df.loc[mask_player, 'pitname']
     
     mask_swing = merged_base_df['swing'] > 0
     if mask_swing.any():
@@ -916,7 +916,7 @@ def stats_df(merged_base_df):
         merged_base_df['approach'] = np.select(condition, choicelist, default='Not Specified')
 
     # 출력할 컬럼 선택
-    stats_output_df = merged_base_df[['game_date', 'player_name', 'pa', 'ab', 'hit', 'walk', 'rel_speed(km)', 
+    stats_output_df = merged_base_df[['game_date', 'pitname', 'pa', 'ab', 'hit', 'walk', 'rel_speed(km)', 
                                      'inplay_pit', 'exit_velocity', 'launch_angleX', 'hit_spin_rate', 
                                      'avg', 'obp', 'slg', 'ops', 'z%', 'z_swing%', 'z_con%', 'z_inplay%', 
                                      'o%', 'o_swing%', 'o_con%', 'o_inplay%', 'f_swing%', 'swing%', 'whiff%', 
